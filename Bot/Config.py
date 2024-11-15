a5f39de1b750087a8fa684ed4a08e5d42f8e39e3
BlingCc <54300418+BlingCc233@users.noreply.github.com>
2024-11-15 20:07:03 +0800

    Create Config.py

---
+++ Bot/Config.py
@@ -0,0 +1,14 @@
+import os
+
+debug_mode = os.environ.get('DEBUG_BOT','True') == 'True'
+
+
+if debug_mode:
+    group_id = 701436956
+    user_id = 1342171891
+
+user_white_list = [1342171891]
+group_white_list = [701436956,398989654]
+
+
+
